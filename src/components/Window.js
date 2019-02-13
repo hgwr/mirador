74a53ff94d13161ae76c9ff9ecc12097079af51b
Andrew Winget (Standard) <scipioaffricanus@gmail.com>
2019-02-13 14:58:45 -0800

    refactor window wrapper to be part of workspaceElastic module

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Rnd } from 'react-rnd';
 import ns from '../config/css-ns';
 import WindowTopBar from '../containers/WindowTopBar';
 import WindowMiddleContent from '../containers/WindowMiddleContent';
@@ -38,64 +37,16 @@ class Window extends Component {
       </div>
     );
   }
-
-  /**
-  * Represents a Window in the mirador workspace
-  * @param {object} window
-  */
-  draggableWorkspaceWindow() {
-    const {
-      window,
-      updateWindowPosition,
-      setWindowSize,
-    } = this.props;
-
-    return (
-      <Rnd
-        size={{ width: window.width, height: window.height }}
-        position={{ x: window.x, y: window.y }}
-        bounds="parent"
-        onDragStop={(e, d) => {
-          updateWindowPosition({ x: d.x, y: d.y });
-        }}
-        onResize={(e, direction, ref, delta, position) => {
-          setWindowSize({
-            width: ref.style.width,
-            height: ref.style.height,
-            ...position,
-          });
-        }}
-        dragHandleClassName={ns('window-top-bar')}
-      >
-        { this.baseWindow() }
-      </Rnd>
-    );
-  }
-
-  /**
-   * Renders things
-   */
-  render() {
-    const { workspaceType } = this.props;
-    return workspaceType === 'elastic'
-      ? this.draggableWorkspaceWindow() : this.baseWindow();
-  }
 }
 
 Window.propTypes = {
   window: PropTypes.object, // eslint-disable-line react/forbid-prop-types
   manifest: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-  updateWindowPosition: PropTypes.func,
-  setWindowSize: PropTypes.func,
-  workspaceType: PropTypes.string,
 };
 
 Window.defaultProps = {
   window: null,
   manifest: null,
-  updateWindowPosition: () => {},
-  setWindowSize: () => {},
-  workspaceType: '',
 };
 
 export default Window;
